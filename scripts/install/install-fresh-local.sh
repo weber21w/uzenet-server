3ddc176aeef942cae48aec7963fa8da8d940c4d2
Lee Weber <weber21w@gmail.com>
2023-05-11 18:33:45 -0500

    Add files via upload

--- scripts/install/install-fresh-local.sh
+++ scripts/install/install-fresh-local.sh
@@ -2,12 +2,17 @@
 cd $(dirname $0)
 
 DOMAIN_NAME="$1"
+SUDOHACK="$2" #don't do this...
+echo \"$SUDOHACK\" | sudo -S echo \"\"
+sleep 1
+
+
 if [[ -z "$DOMAIN_NAME" ]]; then DOMAIN_NAME='uzenet.us'; fi #TODO REMOVE THIS
-UNS_GITHUB_URL="$2"
+UNS_GITHUB_URL="$3"
 if [[ -z "$UNS_GITHUB_URL" ]]; then UNS_GITHUB_URL='https://github.com/weber21w/uzenet-server/archive/refs/heads/main.zip'; fi
-CUZEBOX_GITHUB_URL="$3" #need ESP8266 version, with headless mode
+CUZEBOX_GITHUB_URL="$4" #need ESP8266 version, with headless mode
 if [[ -z "$CUZEBOX_GITHUB_URL" ]]; then CUZEBOX_GITHUB_URL='https://github.com/weber21w/cuzebox-esp8266/archive/refs/heads/main.zip'; fi
-ENABLE_SHELLINABOX="$4"
+ENABLE_SHELLINABOX="$5"
 if [[ -z "$DOMAIN_NAME" ]]; then echo "ERROR requires [DOMAIN-NAME] [OPT-UNS-GITHUB-ZIP-URL] [OPT-CUZEBOX-GITHUB-ZIP-URL]"; exit 1; fi
 
 CUSTOM_SHELLINABOX_PORT="4200"
@@ -27,31 +32,44 @@ RES=$(dpkg -s libsdl2-dev)
 if [[ ! "$RES" == *'installed'* ]]; then echo "**NEED SUDO TO INSTALL [libsdl2-dev]**"; sudo echo ""; sudo apt install -y libsdl2-dev; fi
 RES=$(dpkg -s sshpass)
 if [[ ! "$RES" == *'installed'* ]]; then echo "**NEED SUDO TO INSTALL [sshpass]**"; sudo echo ""; sudo apt install -y sshpass; fi
-
+RES=$(dpkg -s git)
+if [[ ! "$RES" == *'installed'* ]]; then echo "**NEED SUDO TO INSTALL [git]**"; sudo echo ""; sudo apt install -y git; fi
+RES=$(dpkg -s perl)
+if [[ ! "$RES" == *'installed'* ]]; then echo "**NEED SUDO TO INSTALL [perl]**"; sudo echo ""; sudo apt install -y perl; fi
+RES=$(dpkg -s g++)
+if [[ ! "$RES" == *'installed'* ]]; then echo "**NEED SUDO TO INSTALL [g++]**"; sudo echo ""; sudo apt install -y g++; fi
 
 
 ########Install UNS########
 wget -O uns-latest.zip ${UNS_GITHUB_URL}
 unzip -o uns-latest.zip #overwrite existing
 rm uns-latest.zip
+if [[ -d 'uns' ]] #already an existing install?
+then
+	mv uns/data/users.dat users.dat #save existing user file
+	sudo rm -r uns #delete old server version
+fi
 mv *uze* uns #make standard name
 cd uns
 make
 
 #create a systemd service at boot
+if [[ -f '/etc/systemd/system/uzenet-server.service' ]]
+then
+	sudo rm /etc/systemd/system/uzenet-server.service
+fi
 echo "**NEED SUDO TO INSTALL [uzenet-server]**"; sudo echo "";
-sudo echo '
-[Unit]
-Description=Uzenet Server
+echo "[Unit]" | sudo tee -a /etc/systemd/system/uzenet-server.service
+echo "Description=Uzenet Server" | sudo tee -a /etc/systemd/system/uzenet-server.service
+echo "After=syslog.target network.target auditd.service" | sudo tee -a /etc/systemd/system/uzenet-server.service
+echo "" | sudo tee -a /etc/systemd/system/uzenet-server.service
+echo "[Service]" | sudo tee -a /etc/systemd/system/uzenet-server.service
+echo "ExecStart=~/home/uzenet/uns/uns" | sudo tee -a /etc/systemd/system/uzenet-server.service
+echo "" | sudo tee -a /etc/systemd/system/uzenet-server.service
+echo "[Install]" | sudo tee -a /etc/systemd/system/uzenet-server.service
+echo "WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/uzenet-server.service
 
-[Service]
-ExecStart=/home/uzenet/uns/uns
-
-[Install]
-WantedBy=multi-user.target
-' | sudo tee '/etc/systemd/system/uzenet-server.service'
-
-systemctl enable uzenet-server #enable uzenet server to run at boot
+sudo systemctl enable uzenet-server #enable uzenet server to run at boot
 sudo systemctl start uzenet-server #start immediately
 
 
@@ -59,10 +77,18 @@ sudo systemctl start uzenet-server #start immediately
 ########Install CUzeBox########
 rm -r *cuze* #get rid of any old stuff
 
+if [[ -f 'cuzebox-latest.zip' ]]
+then
+	rm 'cuzebox-latest.zip'
+fi
 wget -O cuzebox-latest.zip ${CUZEBOX_GITHUB_URL}
 unzip -o cuzebox-latest.zip #overwrite existing
 
 rm cuzebox-latest.zip
+if [[ -d 'cuzebox' ]]
+then
+	sudo rm -r 'cuzebox'
+fi
 mv *cuze* cuzebox #make standard name
 cd cuzebox
 
@@ -75,23 +101,36 @@ cd ~
 
 ########Install Caddy lightweight static webserver(with automatic SSL)########
 RES=$(dpkg -s caddy)
-if [[ "$RES" == *'installed'* ]]; then sudo systemctl stop caddy; sudo apt remove -y caddy; fi
+if [[ "$RES" == *'installed'* ]]
+then
+	sudo rm /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+	sudo rm /etc/apt/sources.list.d/caddy-stable.list
+	sudo systemctl stop caddy; sudo apt remove -y caddy;
+fi
 
 echo "**NEED SUDO TO INSTALL [caddy]**"; sudo echo "";
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+if [[ ! -f '/usr/share/keyrings/caddy-stable-archive-keyring.gpg' ]]
+then
+	curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+fi
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update -y
 sudo apt install -y caddy
 
-DOMAIN_NAME="$1"
+
 if [[ -z "$DOMAIN_NAME" ]]; then echo "ERROR must specify domain name, ie \"uzenet.us\""; exit 1; fi
 
 
-CFG_DATA=$(cat '/etc/caddy/Caddyfile') #get cfg file data
-CFG_DATA=$(echo "$CFG_DATA" | sed "s/:80 {/${DOMAIN_NAME} {/g") #set our domain(so SSL will work)
-CFG_DATA=$(echo "$CFG_DATA" | sed "s/root \* \/usr\/share\/caddy/root \* \/var\/www\/html/g")
-echo "$CFG_DATA" | sudo tee '/etc/caddy/Caddyfile'
+
+#CFG_DATA=$(cat '/etc/caddy/Caddyfile') #get cfg file data
+#CFG_DATA=$(echo "$CFG_DATA" | sed "s/:80 {/${DOMAIN_NAME} {/g") #set our domain(so SSL will work)
+#CFG_DATA=$(echo "$CFG_DATA" | sed "s/root \* \/usr\/share\/caddy/root \* \/var\/www\/html/g")
+#echo "$CFG_DATA" | sudo tee '/etc/caddy/Caddyfile'
+if [[ -f '/etc/caddy/Caddyfile' ]]
+then
+	sudo rm '/etc/caddy/Caddyfile'
+fi
 
 echo "
 ${DOMAIN_NAME} {
@@ -119,7 +158,11 @@ then
 	then
 		sudo sed -i "/SHELLINABOX_PORT=4200/c\SHELLINABOX_PORT=$NEW_PORT" /etc/default/shellinabox
 	fi
+else
+	sudo apt remove -y shellinabox
 fi
 
 sudo service shellinabox stop
 sudo service shellinabox start
+
+#Optionally Install InspIRCd
